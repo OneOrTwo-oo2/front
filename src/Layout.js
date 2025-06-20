edbec8cb957c3728f614c6a3ce907000100e7c9a
DESKTOP-MNQV98N\songhyun <songhyun0323@konkuk.ac.kr>
2025-06-20 16:32:27 +0900

    edit Layout

--- src/Layout.js
+++ src/Layout.js
@@ -1,10 +1,13 @@
 import Header from './components/Header';
 import { Outlet } from 'react-router-dom';
+import './Layout.css';
+
 
 function Layout() {
   return (
     <>
       <Header />
+      <div className="header-spacer" />
       <main className="page-content">
         <Outlet />
       </main>
