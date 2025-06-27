97dd40726c1c186f29fb8dd313701dbf1907d22d
shin <pinkhong21@gmail.com>
2025-06-27 17:17:23 +0900

    dev Login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './Layout'; // ✅ 추가
+import Layout from './Layout';
 
 // page import
+import LoginPage from './pages/LoginPage'; // ✅ 새로 추가한 로그인 페이지
 import MainPage from './pages/MainPage';
 import IngredientSearchPage from './pages/IngredientSearchPage';
 import PhotoSearchPage from './pages/PhotoSearchPage';
 import Home from './pages/Home';
 import MyInfo from './pages/MyinfoPage';
-import RecipeListPage from './pages/RecipeListPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* ✅ Layout을 모든 페이지에 공통으로 적용 */}
+        {/* ✅ 로그인 페이지는 Layout 없이 보여줌 */}
+        <Route path="/" element={<LoginPage />} />
+
+        {/* ✅ 나머지는 Layout이 공통으로 적용됨 */}
         <Route element={<Layout />}>
-          <Route path="/" element={<MainPage />} />
+          <Route path="/main" element={<MainPage />} />
           <Route path="/ingredient-search" element={<IngredientSearchPage />} />
           <Route path="/photo-search" element={<PhotoSearchPage />} />
           <Route path="/home" element={<Home />} />
           <Route path="/myinfo" element={<MyInfo />} />
-          <Route path="/recipes" element={<RecipeListPage />} />
         </Route>
       </Routes>
     </Router>
