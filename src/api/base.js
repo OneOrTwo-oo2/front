5218f7cf9b5153ac4f72b1b26af7bb804331652b
shin <tlsdud211@gmail.com>
2025-07-22 14:33:51 +0900

    a

--- src/api/base.js
+++ src/api/base.js
@@ -1,2 +1,2 @@
-export const BASE_API = window._env_?.BASE_API || "http://localhost:8000";
-export const AI_API = window._env_?.AI_API || "http://localhost:8001";
+export const BASE_API = window._env_?.BASE_API || "http://localhost:8000";   //  로컬용
+export const AI_API = window._env_?.AI_API || "http://localhost:8001";       // 로컬용
