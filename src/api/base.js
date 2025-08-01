04a17fc6c3a681f01a296acebfd4b5248ae452d4
shin <tlsdud211@gmail.com>
2025-08-01 10:13:40 +0900

    change icons

--- src/api/base.js
+++ src/api/base.js
@@ -1,9 +1,9 @@
 export const getBaseApi = () => {
-    // return "/api";
-    return "http://localhost:8000/api" // 로컬용
+    return "/api";
+    // return "http://localhost:8000/api" // 로컬용
 };
 
 export const getAiApi = () => {
-    // return "/ai";
-    return "http://localhost:8001/ai" // 로컬용
+    return "/ai";
+    // return "http://localhost:8001/ai" // 로컬용
 };
